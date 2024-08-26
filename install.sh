5d839953146c349b3cd3f587bffe69a1baadd84d
m13hack <119548847+m13hack@users.noreply.github.com>
2024-08-26 16:14:31 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "[+] Starting the installation process..."
+
+# Update package lists
+sudo apt-get update
+
+# Install Tor
+echo "[+] Installing Tor..."
+sudo apt-get install -y tor
+
+# Install Python 3 and pip
+echo "[+] Installing Python3 and pip..."
+sudo apt-get install -y python3 python3-pip
+
+# Install Python dependencies
+echo "[+] Installing Python dependencies..."
+pip3 install requests stem spacy reportlab argparse
+
+# Download and install Spacy language model
+echo "[+] Installing Spacy language model..."
+python3 -m spacy download en_core_web_sm
+
+echo "[+] Installation complete. You're ready to use Tor-X."
+
+exit 0
